81e58455d4467f491ad0fee41855d3f156f5f026
GiovannyCortes <giovannyandres.cortes@outlook.com>
2022-12-05 12:55:03 +0100

    Primera prueba de Gio

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {
+          /* Gio está haciendo una prueba de Git */
+        }
         <a
           className="App-link"
           href="https://reactjs.org"

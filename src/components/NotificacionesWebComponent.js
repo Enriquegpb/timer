1c893d5dd9c99c0851824bc9a30a253f805cb16e
Enrique <enriquegpb5@gmail.com>
2022-12-07 14:17:24 +0100

    Pasar a minutos la notificacion

--- src/components/NotificacionesWebComponent.js
+++ src/components/NotificacionesWebComponent.js
@@ -1,76 +1,87 @@
-import React, { Component } from 'react'
-import { ToastContainer,toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { Component } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export default class NotificacionesWebComponent extends Component {
-  notificacionTimer=()=>{
-    var time=900;
-    while(time!==0){
-      if(time<300 && time>60){
-        if(time===175){
-        toast.warning('Quedan menos de: '+time+ ' segundos',{
-          position:"top-right",
-          autoClose:1000,
-          hideProgressBar:false,
-          closeOnClick:true,
-          pauseOnHover:true,
-          draggable:true,
-          progress: undefined,
-          
-      });
-    }
-      }else if(time<=60){
-        if(time===60){
-        toast.error('Finalizando reunión en: '+time+' segundos',{
-          position:"top-right",
-          autoClose:1000,
-          hideProgressBar:false,
-          closeOnClick:true,
-          pauseOnHover:true,
-          draggable:true,
-          progress: undefined,
-          
-      });
-    }
-      }else{
-        if (time===600){
-        toast.success('Quedan: '+time+' segundos',{
-          position:"top-right",
-          autoClose:1000,
-          hideProgressBar:false,
-          closeOnClick:true,
-          pauseOnHover:true,
-          draggable:true,
-          progress: undefined,
-          
-      });
-    }
+  // const delay = (n) => new Promise( r => setTimeout(r, n*1000));
+  /**
+   * Prueba del delay
+   */
+  notificacionTimer = () => {
+    var time = 900;
+    while (time !== 0) {
+      if (time <= 300 && time > 60) {
+        if (time === 300) {
+          toast.warning(
+            "Quedan menos de: " + Math.round(time / 60) + " minutos",
+            {
+              position: "top-right",
+              autoClose: 1000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+            }
+          );
+        }
+        // this.delay(20);
+      } else if (time <= 60) {
+        if (time === 60) {
+          toast.error(
+            "Finalizando reunión en: " + Math.round(time / 60) + "  minuto",
+            {
+              position: "top-right",
+              autoClose: 1000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+            }
+          );
+          // this.delay(20);
+        }
+      } else {
+        if (time === 600) {
+          toast.success("Quedan: " + Math.round(time / 60) + " minutos", {
+            position: "top-right",
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        }
+        // this.delay(20);
       }
 
-      time-=1;
+      time -= 1;
       console.log(time);
     }
-    
-  } 
-    componentDidMount=()=>{
-      toast.success('Comineza la reunión',{
-        position:"top-right",
-        autoClose:1000,
-        hideProgressBar:false,
-        closeOnClick:true,
-        pauseOnHover:true,
-        draggable:true,
-        progress: undefined,
-        
+  };
+  // delay = (n) => {
+  //   return new Promise(function (resolve) {
+  //     setTimeout(resolve, n * 1000);
+  //   });
+  // };
+  componentDidMount = () => {
+    toast.success("Comineza la reunión", {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
     });
     this.notificacionTimer();
-   
-  }
+  };
   render() {
     return (
       <div>
-        <ToastContainer/>
+        <ToastContainer />
       </div>
-      
-    )
+    );
   }
 }

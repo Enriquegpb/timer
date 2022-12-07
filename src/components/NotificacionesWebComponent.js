86a47111a9ce4359a27d16f0d6d9b65f05dc2530
Enrique <enriquegpb5@gmail.com>
2022-12-07 10:24:50 +0100

    Control de resultado de la notificación web

--- src/components/NotificacionesWebComponent.js
+++ src/components/NotificacionesWebComponent.js
@@ -2,17 +2,69 @@ import React, { Component } from 'react'
 import { ToastContainer,toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 export default class NotificacionesWebComponent extends Component {
+  notificacionTimer=()=>{
+    var time=900;
+    while(time!==0){
+      if(time<300 && time>60){
+        if(time===175){
+        toast.warning('Quedan menos de: '+time+ ' segundos',{
+          position:"top-right",
+          autoClose:1000,
+          hideProgressBar:false,
+          closeOnClick:true,
+          pauseOnHover:true,
+          draggable:true,
+          progress: undefined,
+          
+      });
+    }
+      }else if(time<=60){
+        if(time===60){
+        toast.error('Finalizando reunión en: '+time+' segundos',{
+          position:"top-right",
+          autoClose:1000,
+          hideProgressBar:false,
+          closeOnClick:true,
+          pauseOnHover:true,
+          draggable:true,
+          progress: undefined,
+          
+      });
+    }
+      }else{
+        if (time===600){
+        toast.success('Quedan: '+time+' segundos',{
+          position:"top-right",
+          autoClose:1000,
+          hideProgressBar:false,
+          closeOnClick:true,
+          pauseOnHover:true,
+          draggable:true,
+          progress: undefined,
+          
+      });
+    }
+      }
+
+      time-=1;
+      console.log(time);
+    }
+    
+  } 
     componentDidMount=()=>{
-      toast('Faltan 15 segundos',{
+      toast.success('Comineza la reunión',{
         position:"top-right",
-        autoClose:10000,
+        autoClose:1000,
         hideProgressBar:false,
         closeOnClick:true,
         pauseOnHover:true,
         draggable:true,
         progress: undefined,
+        
     });
-    }
+    this.notificacionTimer();
+   
+  }
   render() {
     return (
       <div>

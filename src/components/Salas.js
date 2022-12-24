a8f9df20c286b1d2119915ac79bc0d22871abd2b
GiovannyCortes <giovannyandres.cortes@outlook.com>
2022-12-24 19:54:36 +0100

    Agregada -> Galería flex, nuevas salas. Working...

--- src/components/Salas.js
+++ src/components/Salas.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
+import './css/Salas.css';
 
 import Menu from './Menu';
+import plusicon from '../assets/plus.svg';
 
 export class Salas extends Component {
+
+    state = {
+        salas : ["TechnoSala", "Azure MetaSala Que agí vaaaaaaaaaa leñeeee vamossss daleeeeee fiuuuuu joeeeeee", "Office 365 Sala", "La Ultra Mega Sala tech riders vulcanoname"]
+    }
+
     render() {
       return (
             <div>
                 <div className='noselect' style={{"marginTop":"10px"}}>
                     <Menu />
                 </div>
-                Salas
+                <div className='content_box'>
+                    {
+                        this.state.salas.map((sala, index) => {
+                            return (
+                                <div className='box_sala' key={index}>
+                                    <p className='box_sala_target noselect'>
+                                        {sala}
+                                    </p>
+                                </div>
+                            )
+                        })
+                    }
+                    <div className='box_sala last_item'>
+                        <p className='box_sala_target noselect'>
+                            <img src={plusicon} alt="Icono más" className='plusicon'/>
+                        </p>
+                    </div>
+                </div>
             </div>
       )
     }
 }
 
-export default Salas
+export default Salas;

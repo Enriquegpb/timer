2c4a15a6adc0a9f4042b6348fb0557c0decc0f4d
GiovannyCortes <giovannyandres.cortes@outlook.com>
2022-12-27 12:13:40 +0100

    Agregado doble input en categorías

--- src/components/Categorias.js
+++ src/components/Categorias.js
@@ -13,7 +13,16 @@ export class Categorias extends Component {
             categorías almacenadas en la BBDD.
         */
         this.setState({
-            categorias : ["Categoría 1", "Categoría 2"]
+            categorias : [
+                {
+                    "name" : "CATEGORÍA 1",
+                    "duration" : "00:15"
+                },
+                {
+                    "name" : "Categoría 2",
+                    "duration" : "00:30"
+                }
+            ]
         });
     }
 
@@ -22,26 +31,55 @@ export class Categorias extends Component {
         categorias : []
     }
 
+
     generateCategories = () => {
-        Swal.fire({
+        new Swal({
             title: 'Nueva categoría',
-            input: 'text',
-            inputLabel: 'Inserte un nuevo nombre para la categoría',
+            html:
+                '<label for="swal-input1">Nombre</label></br>' +
+                '<input id="swal-input1" class="swal2-input" style="margin-top:5px;margin-bottom:0;"/></br>' +
+                '<p id="error_1" style="display:none; color:red;">El nombre no puede estar vacío</p>' +
+                '<p id="error_2" style="display:none; color:red;">Ya existe una categoría con el mismo nombre</p></br>' +
+                '<label for="swal-input2">Duración</label></br>' +
+                '<input type="time" id="swal-input2" class="swal2-input" value="00:15" style="margin-top:5px;"/></br>' +
+                '<p id="error_3" style="display:none; color:red; margin-bottom:0;">Tiempo mínimo: 1 minuto</p>',
+            focusConfirm: false,
             showCancelButton: true,
             confirmButtonText: "Aceptar",
+            confirmButtonColor: '#2C4D9E',
             cancelButtonText: "Cancelar",
-            inputValidator: (value) => {
-                if (!value) {
-                    return 'Debe añadir contenido para crear una nueva categoría';
-                } else {
+            preConfirm: () => {
+                if(document.getElementById('swal-input1').value) {
                     var auxiliar = this.state.categorias;
                     var correcto = true;
                     auxiliar.forEach(element => {
-                        if (value.toUpperCase() === element.toUpperCase()) {
+                        if (document.getElementById('swal-input1').value.toUpperCase() === element.name.toUpperCase()) {
                             correcto = false;       
                         }
                     });
-                    if (!correcto) { return 'Ya existe una categoría con el mismo nombre' };
+                    if (!correcto) { 
+                        document.getElementById('error_1').style.display = 'none';
+                        document.getElementById('error_2').style.display = 'inline-block';
+                        document.getElementById('error_3').style.display = 'none';
+                        return false; 
+                    };
+                }
+
+                if(!document.getElementById('swal-input1').value){
+                    document.getElementById('error_1').style.display = 'inline-block';
+                    document.getElementById('error_2').style.display = 'none';
+                    document.getElementById('error_3').style.display = 'none';
+                    return false;
+                } else if(document.getElementById('swal-input2').value === "00:00") {
+                    document.getElementById('error_1').style.display = 'none';
+                    document.getElementById('error_2').style.display = 'none';
+                    document.getElementById('error_3').style.display = 'inline-block';
+                    return false;
+                } else {
+                    return [
+                        document.getElementById('swal-input1').value,
+                        document.getElementById('swal-input2').value
+                    ]
                 }
             }
         }).then((result) => {
@@ -50,8 +88,12 @@ export class Categorias extends Component {
                     #2 (GIO) TO (GUTI/SERGIO) ->
                     Resumen: Prepara esta zona para agregar la nueva categoría en la BBDD.
                 */
+                var newCategory = {
+                    name : result.value[0],
+                    duration : result.value[1]
+                }
                 var auxiliar = this.state.categorias;
-                    auxiliar.push(result.value);
+                    auxiliar.push(newCategory);
                 this.setState({
                     categorias : auxiliar
                 })
@@ -61,26 +103,38 @@ export class Categorias extends Component {
     }
 
     modifyCategory = (index) => {
-        Swal.fire({
+        new Swal({
             title: 'Modificar categoría',
-            input: 'text',
-            inputLabel: 'Inserte un nuevo nombre para la categoría',
-            inputValue: this.state.categorias[index],
+            html:
+                '<label for="swal-input1">Nombre</label></br>' +
+                '<input id="swal-input1" class="swal2-input" style="margin-top:5px;margin-bottom:0;" value="' + 
+                this.state.categorias[index].name + 
+                '"/></br>' +
+                '<p id="error_1" style="display:none; color:red;">El nombre no puede estar vacío</p>' +
+                '<label for="swal-input2">Duración</label></br>' +
+                '<input type="time" id="swal-input2" class="swal2-input" value="' + 
+                this.state.categorias[index].duration + 
+                '" style="margin-top:5px;"/></br>' +
+                '<p id="error_2" style="display:none; color:red; margin-bottom:0;">Tiempo mínimo: 1 minuto</p>',
+            focusConfirm: false,
             showCancelButton: true,
             confirmButtonText: "Aceptar",
+            confirmButtonColor: '#2C4D9E',
             cancelButtonText: "Cancelar",
-            inputValidator: (value) => {
-                if (!value) {
-                    return 'El nuevo nombre de la categoría no debe estar en blanco';
+            preConfirm: () => {
+                if(!document.getElementById('swal-input1').value){
+                    document.getElementById('error_1').style.display = 'inline-block';
+                    document.getElementById('error_2').style.display = 'none';
+                    return false;
+                } else if(document.getElementById('swal-input2').value === "00:00") {
+                    document.getElementById('error_1').style.display = 'none';
+                    document.getElementById('error_2').style.display = 'inline-block';
+                    return false;
                 } else {
-                    var auxiliar = this.state.categorias;
-                    var correcto = true;
-                    auxiliar.forEach(element => {
-                        if (value.toUpperCase() === element.toUpperCase()) {
-                            correcto = false;       
-                        }
-                    });
-                    if (!correcto) { return 'Ya existe una categoría con el mismo nombre' };
+                    return [
+                        document.getElementById('swal-input1').value,
+                        document.getElementById('swal-input2').value
+                    ]
                 }
             }
         }).then((result) => {
@@ -89,11 +143,16 @@ export class Categorias extends Component {
                     #3 (GIO) TO (GUTI/SERGIO) ->
                     Resumen: Prepara esta zona para actualizar la categoría en la BBDD.
                 */
+                var newCategory = {
+                    name : result.value[0],
+                    duration : result.value[1]
+                }
                 var auxiliar = this.state.categorias;
-                    auxiliar.fill(result.value, index, index+1);
+                    auxiliar.fill(newCategory, index, index+1);
                 this.setState({
                     categorias : auxiliar
                 })
+                console.log(this.state.categorias);
             }
         });
     }
@@ -108,8 +167,17 @@ export class Categorias extends Component {
                             return (
                                 <div className='box_categoria' key={index} onClick={() => this.modifyCategory(index)}>
                                     <p className='box_categoria_target noselect'>
-                                        {categoria}
-                                    </p>
+                                        {categoria.name}
+                                    </p> <br/>
+                                    {
+                                        categoria.name.length > 12 && categoria.name.split().length > 1 ?
+                                        (
+                                            <p style={{"margin":"0"}}>{categoria.duration}</p>
+                                        ) :
+                                        (
+                                            <p style={{"marginTop":"10px", "marginBottom":"0"}}>{categoria.duration}</p>
+                                        )
+                                    }
                                 </div>
                             )
                         })

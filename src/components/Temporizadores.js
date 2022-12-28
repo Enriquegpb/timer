f61af82545203af830f38494258a275bf879f274
GiovannyCortes <giovannyandres.cortes@outlook.com>
2022-12-28 18:52:33 +0100

    Método de creación de temporizadores, completado

--- src/components/Temporizadores.js
+++ src/components/Temporizadores.js
@@ -5,11 +5,13 @@ import Swal from 'sweetalert2';
 import './css/Temporizadores.css';
 export class Temporizadores extends Component {
     state = {
-        temporizadores : []
+        temporizadores : [],
+        categorias : []
     }
 
     componentDidMount = () => {
         this.loadTimers();
+        this.loadCategories();
     }
 
     loadTimers = () => {
@@ -18,28 +20,118 @@ export class Temporizadores extends Component {
             Resumen: Prepara el método para cargar los
                      temporizadores almacenados en la BBDD.
         */
-            this.setState({
-                temporizadores : [
-                    {
-                        idtimer : 1,
-                        inicio : "08:30",
-                        idcategoria : 1, // SUPONGAMOS QUE ES 'WORK'
-                        pausa : false // No necesario
-                    },
-                    {
-                        idtimer : 1,
-                        inicio : "08:45",
-                        idcategoria : 2, // SUPONGAMOS QUE ES 'BREAK 5MIN'
-                        pausa : false // No necesario
-                    },
-                    {
-                        idtimer : 1,
-                        inicio : "08:50",
-                        idcategoria : 3, // SUPONGAMOS QUE ES 'LONG BREAK'
-                        pausa : false // No necesario
-                    }
-                ]
-            });
+        this.setState({
+            temporizadores : [
+                {
+                    idtimer : 1,
+                    inicio : "08:30",
+                    idcategoria : 1, // SUPONGAMOS QUE ES 'WORK'
+                    pausa : false // No necesario
+                },
+                {
+                    idtimer : 1,
+                    inicio : "08:45",
+                    idcategoria : 2, // SUPONGAMOS QUE ES 'BREAK 5MIN'
+                    pausa : false // No necesario
+                },
+                {
+                    idtimer : 1,
+                    inicio : "08:50",
+                    idcategoria : 3, // SUPONGAMOS QUE ES 'LONG BREAK'
+                    pausa : false // No necesario
+                }
+            ]
+        });
+    }
+
+    loadCategories = () => {
+        /*
+            #2 (GIO) TO (GUTI/SERGIO) ->
+            Resumen: Prepara el método para cargar las
+                     categorias almacenados en la BBDD.
+        */
+        this.setState({
+            categorias : [
+                {
+                    idcategoria : 1,
+                    categoria : "CATEGO_1",
+                    duracion : "00:15"
+                },
+                {
+                    idcategoria : 2,
+                    categoria : "CATEGO_2",
+                    duracion : "00:05"
+                },
+                {
+                    idcategoria : 3,
+                    categoria : "CATEGO_3",
+                    duracion : "00:30"
+                }
+            ]
+        });
+    }
+
+    getOptionsCategories = () => {
+        var auxiliar = '';
+        this.state.categorias.forEach((catego) => {
+            auxiliar += '<option value="' + catego.idcategoria + '">' + 
+                            catego.categoria +
+                        '</option>';
+        })
+        return auxiliar;
+    }
+
+    generateTimer = () => {
+        new Swal({
+            title: 'Nuevo temporizador',
+            html:
+                '<label for="swal-input1">Hora de inicio</label></br>' +
+                '<input type="time" id="swal-input1" class="swal2-input" style="margin-top:5px;"/></br>' +
+                '<p id="error_1" style="display:none; color:red;">Por favor, inserte una hora válida</p></br>' +
+                '<label for="swal-input2">Categoría</label></br>' +
+                '<select  id="swal-input2" class="swal2-input" style="margin-top:5px; width:70%;">' + 
+                this.getOptionsCategories() + 
+                '</select>',
+            focusConfirm: false,
+            showCancelButton: true,
+            confirmButtonText: "Aceptar",
+            confirmButtonColor: '#2C4D9E',
+            cancelButtonText: "Cancelar",
+            preConfirm: () => {
+                if (document.getElementById('swal-input1').value === "") {
+                    document.getElementById('error_1').style.display = 'inline-block';
+                    return false;
+                } else {
+                    return [
+                        document.getElementById('swal-input1').value,
+                        document.getElementById('swal-input2').value
+                    ]
+                }
+            }
+        }).then((result) => {
+            if (result.isConfirmed) {
+                console.log(result);
+                /*
+                    #3 (GIO) TO (GUTI/SERGIO) ->
+                    Resumen: Prepara esta zona para agregar el nuevo temporizador en la BBDD.
+                */
+                var newTimer = {
+                    idtimer : 1000, // Entiendo que todos los id son autogenerados????
+                    inicio : result.value[0],
+                    idcategoria : result.value[1],
+                    pausa : false // No necesario
+                }
+                var auxiliar = this.state.temporizadores;
+                    auxiliar.push(newTimer);
+                this.setState({
+                    temporizadores : auxiliar
+                })
+            }
+        });
+    }
+
+    modifyTimer = () => {
+        
     }
 
     render() {

93feb7f399bf48365b6c3d7fde40c3d7336dadea
GiovannyCortes <giovannyandres.cortes@outlook.com>
2023-01-07 15:33:31 +0100

    Fix Buttons Schedule

--- src/components/Horario.js
+++ src/components/Horario.js
@@ -325,7 +325,7 @@ export class Horario extends Component {
                         <thead>
                             <tr className='schedule_header'>
                                 <th></th>
-                                <th colSpan={4}>
+                                <th colSpan={4} id="theader_schedule">
                                     <button className='schedule_buttons_rooms' onClick={() => this.changeRoom(-1)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
                                             <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
@@ -333,7 +333,7 @@ export class Horario extends Component {
                                     </button>
                                     {
                                         this.state.sala_actual >= 0 && this.state.salas && (
-                                            this.state.salas[this.state.sala_actual].nombreSala
+                                            <p>{this.state.salas[this.state.sala_actual].nombreSala}</p>
                                         )
                                     }
                                     <button className='schedule_buttons_rooms' onClick={() => this.changeRoom(1)}>
